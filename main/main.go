fbaaba3a6bd451c212d4853238973249ebd404ea
visalhok <visalhok123@gmail.com>
2019-03-22 14:31:15 -0700

    Test Server

--- main/main.go
+++ main/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"Naturae_Server/helpers"
-	"Naturae_Server/users"
+	"fmt"
 	"log"
 )
 
@@ -11,11 +11,12 @@ func main() {
 	defer cleanUpServer()
 	//users.Login("visalhok123@gmail.com", "ABab1234!@#")
 	//Connect to all of the services that is needed to run the server
-	email := "visalhok123@gmail.com"
-	firstName := "Visal"
-	lastName := "Hok"
-	password := "ABab1234!@#"
-	users.CreateAccount(email, firstName, lastName, password)
+	fmt.Println(*helpers.GetCurrentDBConnection())
+	//email := "visalhok123@gmail.com"
+	//firstName := "Visal"
+	//lastName := "Hok"
+	//password := "ABab1234!@#"
+	//users.CreateAccount(email, firstName, lastName, password)
 }
 
 //Initialize all of the variable to be uses
