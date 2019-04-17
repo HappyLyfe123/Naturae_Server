a3a5dda16c7a4a0ed1a84b46dc255104b3a8b291
Steven Lim <limstevenlbw@gmail.com>
2019-04-17 11:05:54 -0700

    Search, Add, and Remove Functions

--- users/user.go
+++ users/user.go
@@ -5,7 +5,6 @@ import (
 	pb "Naturae_Server/naturaeproto"
 	"context"
 	"log"
-	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -105,49 +104,146 @@ func saveRefreshToken(database *mongo.Database, token *helpers.RefreshToken) {
 * 	@UserListReply - repeated string users, Status status
  */
 func SearchUsers(request *pb.UserSearchRequest) *pb.UserListReply {
-	var result userAccount
 	var searchResult []string
 
 	dbConnection := helpers.ConnectToDB(helpers.GetUserDatabase())
 	userEmail := request.GetUser()
 
-	//Set a filter for the database to search through()
+	//if userEmail is not nil, it means to retrieve the friendslist of the logged in user
 	if len(userEmail) > 0 {
-		//Get the user account to retrieve the friend's list from
 		userAccount, err := getUserAccountInfo(dbConnection, userEmail)
 		searchResult = userAccount.Friends
+		if err != nil {
+			return &pb.UserListReply{User: nil,
+				Status: &pb.Status{Code: helpers.GetInternalServerErrorStatusCode(), Message: "Failed UserEmail Get"}}
+		}
 	} else {
-		//Search via user query input instead, get all users with the email
+		//Search for exactly one user with an inputted email
 		queryEmail := request.GetQuery()
+		userAccount, err := getUserAccountInfo(dbConnection, queryEmail)
+		searchResult = userAccount.Friends
+		if err != nil {
+			return &pb.UserListReply{User: nil,
+				Status: &pb.Status{Code: helpers.GetInternalServerErrorStatusCode(), Message: "Failed QueryEmail Get"}}
+		}
+		/*Search via user query input instead, get all users with similar email(not working)
 		queryString := strings.Split(queryEmail, " ")
 		querySplice := make([]bson.M, len(queryString))
 		for i, queryEmail := range queryString {
 			querySplice[i] = bson.M{"email": bson.M{
-				"$regex": bson.RegEx{Pattern: ".*" + queryEmail + ".*", Options: "i"},
+				"$regex": bson.Regex{Pattern: ".*" + queryEmail + ".*", Options: "i"},
 			}}
 		}
 
-		//Connect to Account_Information collections
-		userCollection := helpers.ConnectToCollection(dbConnection, helpers.GetAccountInfoCollection())
-		//Make the search request
-		filter := bson.M{"$querySplice": querySplice}
-		//&filter?
-		err := userCollection.Find(nil, &filter).Limit(10).All(&searchResult)
-		if err != nil {
-
 		}
-
-		return &pb.UserListReply{User: searchResult,
-			Status: &pb.Status{Code: 200, Message: "Created"}}
+			//Connect to Account_Information collections
+			userCollection := helpers.ConnectToCollection(dbConnection, helpers.GetAccountInfoCollection())
+			//Make the search request
+			filter := bson.M{"$querySplice": querySplice}
+			//&filter?
+			err := userCollection.Find(nil, &filter).Limit(10).All(&searchResult)
+		*/
 	}
+
+	return &pb.UserListReply{User: searchResult,
+		Status: &pb.Status{Code: helpers.GetOkStatusCode(), Message: "Okay"}}
 }
 
 //Adds a friend to a user's list of contacts
 func AddFriend(request *pb.FriendRequest) *pb.FriendReply {
+	dbConnection := helpers.ConnectToDB(helpers.GetUserDatabase())
+	userCollection := helpers.ConnectToCollection(dbConnection, helpers.GetAccountInfoCollection())
+	//Set a filter for the database to search through
+	senderFilter := bson.D{{Key: "email", Value: request.GetSender()}}
+	receiverFilter := bson.D{{Key: "email", Value: request.GetReceiver()}}
 
+	cherr := make(chan error, 2)
+	//Update Interface, Push Friend Usernames to Friendslist of both users
+	go func() {
+		updateSender := bson.D{
+			{"$push", bson.D{
+				{"Friends", request.GetReceiver()},
+			}},
+		}
+		_, err := userCollection.UpdateOne(
+			context.Background(),
+			senderFilter,
+			updateSender,
+		)
+		if err != nil {
+			cherr <- err
+		}
+	}()
+	go func() {
+		updateReceiver := bson.D{
+			{"$push", bson.D{
+				{"Friends", request.GetSender()},
+			}},
+		}
+		_, err := userCollection.UpdateOne(
+			context.Background(),
+			receiverFilter,
+			updateReceiver,
+		)
+		if err != nil {
+			cherr <- err
+		}
+	}()
+
+	if <-cherr != nil || <-cherr != nil {
+		return &pb.FriendReply{
+			Status: &pb.Status{Code: helpers.GetInternalServerErrorStatusCode(), Message: "Error Adding Friends"}}
+	}
+	return &pb.FriendReply{
+		Status: &pb.Status{Code: helpers.GetOkStatusCode(), Message: "Success"}}
 }
 
 //Removes a friend from a user's list of contacts
 func RemoveFriend(request *pb.FriendRequest) *pb.FriendReply {
+	dbConnection := helpers.ConnectToDB(helpers.GetUserDatabase())
+	userCollection := helpers.ConnectToCollection(dbConnection, helpers.GetAccountInfoCollection())
+	//Set a filter for the database to search through
+	senderFilter := bson.D{{Key: "email", Value: request.GetSender()}}
+	receiverFilter := bson.D{{Key: "email", Value: request.GetReceiver()}}
 
+	//Update Interface, Push Friend Usernames to Friendslist of both users
+	cherr := make(chan error, 2)
+
+	go func() {
+		updateSender := bson.D{
+			{"$pull", bson.D{
+				{"Friends", request.GetReceiver()},
+			}},
+		}
+		_, err := userCollection.UpdateOne(
+			context.Background(),
+			senderFilter,
+			updateSender,
+		)
+		if err != nil {
+			cherr <- err
+		}
+	}()
+	go func() {
+		updateReceiver := bson.D{
+			{"$pull", bson.D{
+				{"Friends", request.GetSender()},
+			}},
+		}
+		_, err := userCollection.UpdateOne(
+			context.Background(),
+			receiverFilter,
+			updateReceiver,
+		)
+		if err != nil {
+			cherr <- err
+		}
+	}()
+
+	if <-cherr != nil || <-cherr != nil {
+		return &pb.FriendReply{
+			Status: &pb.Status{Code: helpers.GetInternalServerErrorStatusCode(), Message: "Error Removing Friends"}}
+	}
+	return &pb.FriendReply{
+		Status: &pb.Status{Code: helpers.GetOkStatusCode(), Message: "Success"}}
 }
