58092bb2fe12df616ba492b391ead0e17eac5691
HappyLyfe123 <visalhok123@gmail.com>
2019-04-20 17:36:25 -0700

    Update store post

--- post/store_post.go
+++ post/store_post.go
@@ -24,15 +24,16 @@ func SavePost(request *pb.CreatePostRequest, ownerEmail string) *pb.CreatePostRe
 	connectedDB := helpers.ConnectToDB(helpers.GetPostDatabase())
 	//Create a unique id for the post
 	postID := helpers.CreateUUID()
-
+	//Crate a structure to store the post
 	newPost := postDescription{PostID: postID, OwnerEmail: ownerEmail, Title: request.Title, Species: request.Species, Description: request.Description,
 		Latitude: request.GetLat(), Longitude: request.GetLng(), EncodedImage: request.GetEncodedImage(), Date: time.Now()}
 	postCollection := connectedDB.Collection(helpers.GetStorePostsCollection())
 	_, err := postCollection.InsertOne(context.Background(), newPost)
+	//Check if the post was able to save
 	if err != nil {
 		log.Printf("error while saving post: %v", err)
 		return &pb.CreatePostReply{Status: &pb.Status{Code: helpers.GetInternalServerErrorStatusCode(), Message: "server time out"}}
 	}
-
+	//Post was able to save
 	return &pb.CreatePostReply{Status: &pb.Status{Code: helpers.GetOkStatusCode(), Message: "post saved"}}
 }
